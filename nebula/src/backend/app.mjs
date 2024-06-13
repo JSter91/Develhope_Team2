46bc16e0a785dc420c5469fcb64b8f00f6d29a36
salvatore <marottasalvatore@gmail.com>
2024-06-13 11:02:08 +0200

    added controls

--- nebula/src/backend/app.mjs
+++ nebula/src/backend/app.mjs
@@ -1,5 +1,8 @@
 import express, { json } from "express";
 import cors from "cors";
+import morgan from 'morgan';
+import helmet from 'helmet';
+
 const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
@@ -12,16 +15,22 @@ import userRoutes from './routes/userRoutes.mjs';
 
 app.use(cors());
 app.use(json());
+app.use(morgan('dev'));
+app.use(helmet());
 
 setupDb(); // Setup the database
 
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/users', userRoutes);
 
-// app.use((err, req, res) => {
-//   console.error("Internal server error:", err);
-//   res.status(500).json({ message: "Internal server error" });
-// });
+app.use((err, req, res) => {
+  console.error("Internal server error:", err);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
